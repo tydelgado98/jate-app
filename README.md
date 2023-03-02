2c7cd9fc9130e69e68695c2271f8af5bd7630784
unknown <delgadojustin100@yahoo.com>
2023-03-01 22:21:42 -0500

    finished the app for the most part

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # jate-app
+  (https://img.shields.io/badge/license-mit-blue)
+
+  ## Description
+  
+  - Wanted to make a text editor that anyone can install locally to their pc and make it easy to make code on the go!
+  
+  ## Installation
+  
+  PWA
+  
+
+  ## Email
+
+  delgadojustin100@yahoo.com
+  
+
+  ## GitHub
+
+  tydelgado98
+  
+  ## Credits
+  
+  Justin Delgado
+
+  # Video
+  <!-- [![video recording](videos/teamgen.png)](videos/Team%20Generator.webm) -->
+  
+  ## License
+  
+  My license is the (https://choosealicense.com/licenses/mit) License
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,48 +12,30 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Method that takes some content and adds it to the IndexedDB database using the idb module
 export const putDb = async (content) => {
-  console.log('GET from the database');
-
-  // Create a connection to the database database and version we want to use.
+  console.log('PUT to the database');
   const jateDb = await openDB('jate', 1);
-
-  // Create a new transaction and specify the database and data privileges.
-  const tx = jateDb.transaction('jate', 'readonly');
-
-  // Open up the desired object store.
+  const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-
-  // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
-
-  // Get confirmation of the request.
+  const request = store.put({ id: 1, value: content });
   const result = await request;
-  console.log('result.value', result);
-  return result;
-}
+  console.log('🚀 - data saved to the database', result.value);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
+// Method that gets content from the IndexedDB database using the idb module
 export const getDb = async () => {
   console.log('GET from the database');
-
-  // Create a connection to the database database and version we want to use.
   const jateDb = await openDB('jate', 1);
-
-  // Create a new transaction and specify the database and data privileges.
   const tx = jateDb.transaction('jate', 'readonly');
-
-  // Open up the desired object store.
   const store = tx.objectStore('jate');
-
-  // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
-
-  // Get confirmation of the request.
+  const request = store.get(1);
   const result = await request;
-  console.log('result.value', result);
-  return result;
+  result
+    ? console.log('🚀 - data retrieved from the database', result.value)
+    : console.log('🚀 - data not found in the database');
+  // Check if a variable is defined and if it is, return it. See MDN Docs on Optional Chaining (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining)
+  return result?.value;
 };
 
 initdb();
