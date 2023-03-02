b9188b465208506e2c44ac01aab2d27fcda1288e
unknown <delgadojustin100@yahoo.com>
2023-03-01 21:58:39 -0500

    hopefully this works

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,5 +26,19 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
+registerRoute(
+ 
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+   
+    cacheName: 'asset-cache',
+    plugins: [
+      
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
 // TODO: Implement asset caching
 registerRoute();
